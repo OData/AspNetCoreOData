d0f4d4a77291d0416463f3ca2255d545f521ef10
Sam Xu <saxu@microsoft.com>
2020-07-28 16:42:20 -0700

    Modify the test for path templates

---
+++ src/Microsoft.AspNetCore.OData/Routing/Template/ODataPathTemplateExtensions.cs
@@ -0,0 +1,286 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using Microsoft.OData.Edm;
+
+namespace Microsoft.AspNetCore.OData.Routing.Template
+{
+    /// <summary>
+    /// The extension methods for <see cref="ODataPathTemplate"/>
+    /// </summary>
+    public static class ODataPathTemplateExtensions
+    {
+        /// <summary>
+        /// Gets the whole supported template belongs to a <see cref="ODataPathTemplate"/>.
+        /// We supports:
+        /// 1. Key as segment if it's single key (We doesn't consider the alternate key so far)
+        /// 2. Unqualified function/action call
+        /// 3. Optional parameters combination.
+        /// </summary>
+        /// <param name="path">The input path template.</param>
+        /// <returns>The whole path template string.</returns>
+        public static IEnumerable<string> GetAllTemplates(this ODataPathTemplate path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            IList<StringBuilder> templates = new List<StringBuilder>
+            {
+                new StringBuilder()
+            };
+
+            int index = 0;
+            foreach (ODataSegmentTemplate segment in path.Segments)
+            {
+                if (segment.Kind == ODataSegmentKind.Key)
+                {
+                    KeySegmentTemplate keySg = segment as KeySegmentTemplate;
+                    if (keySg.Count == 1)
+                    {
+                        templates = CombinateTemplates(templates, "(" + segment.Literal + ")", "/" + segment.Literal);
+                    }
+                    else
+                    {
+                        templates = CombinateTemplate(templates, "(" + segment.Literal + ")");
+                    }
+
+                    index++;
+                    continue;
+                }
+
+                if (index != 0)
+                {
+                    templates = CombinateTemplate(templates, "/");
+                }
+                index++;
+
+                if (segment.Kind == ODataSegmentKind.Action)
+                {
+                    ActionSegmentTemplate action = (ActionSegmentTemplate)segment;
+                    templates = CombinateTemplates(templates, action.Action.FullName(), action.Action.Name);
+                }
+                else if (segment.Kind == ODataSegmentKind.Function)
+                {
+                    FunctionSegmentTemplate function = (FunctionSegmentTemplate)segment;
+                    IList<string> functionTemplates = function.Function.GenerateFunctionTemplates();
+                    templates = CombinateTemplates(templates, functionTemplates);
+                }
+                else if (segment.Kind == ODataSegmentKind.FunctionImport)
+                {
+                    FunctionImportSegmentTemplate functionImport = (FunctionImportSegmentTemplate)segment;
+                    IList<string> functionTemplates = functionImport.FunctionImport.Function.GenerateFunctionTemplates();
+                    templates = CombinateTemplates(templates, functionTemplates);
+                }
+                else
+                {
+                    templates = CombinateTemplate(templates, segment.Literal);
+                }
+            }
+
+            return templates.Select(t => t.ToString());
+        }
+
+        /// <summary>
+        /// Generates all templates for an input function.
+        /// </summary>
+        /// <param name="edmFunction">The input function.</param>
+        /// <returns>All templates.</returns>
+        internal static IList<string> GenerateFunctionTemplates(this IEdmFunction edmFunction)
+        {
+            if (edmFunction == null)
+            {
+                throw new ArgumentNullException(nameof(edmFunction));
+            }
+
+            // split parameters
+            (var fixes, var optionals) = SplitParameters(edmFunction);
+
+            // gets all combinations of the optional parameters.
+            Stack<IEdmOptionalParameter> current = new Stack<IEdmOptionalParameter>();
+            IList<IEdmOptionalParameter[]> full = new List<IEdmOptionalParameter[]>();
+            int length = optionals.Count;
+            if (optionals.Count > 0)
+            {
+                Traveral(optionals.ToArray(), 0, length, current, full);
+            }
+
+            IList<string> functionNameLists = new List<string>();
+            if (edmFunction.IsBound)
+            {
+                functionNameLists.Add(edmFunction.FullName());
+                functionNameLists.Add(edmFunction.Name);
+            }
+            else
+            {
+                functionNameLists.Add(edmFunction.Name);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            IList<string> functionLiterals = new List<string>();
+            foreach (var fullName in functionNameLists)
+            {
+                sb.Clear();
+                sb.Append(fullName).Append("(");
+
+                bool isFirst = true;
+                foreach (var fixParam in fixes)
+                {
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                    }
+                    else
+                    {
+                        sb.Append(",");
+                    }
+
+                    sb.Append($"{fixParam.Name}={{{fixParam.Name}}}");
+                }
+
+                if (full.Any())
+                {
+                    string pathSofar = sb.ToString();
+                    foreach (var optional in full)
+                    {
+                        sb.Clear();
+                        foreach (var item in optional)
+                        {
+                            if (isFirst)
+                            {
+                                isFirst = false;
+                            }
+                            else
+                            {
+                                sb.Append(",");
+                            }
+
+                            sb.Append($"{item.Name}={{{item.Name}}}");
+                        }
+                        sb.Append(")");
+                        functionLiterals.Add(pathSofar + sb.ToString());
+                    }
+                }
+                else
+                {
+                    sb.Append(")");
+                    functionLiterals.Add(sb.ToString());
+                }
+            }
+
+            return functionLiterals;
+        }
+
+        /// <summary>
+        /// Traveral the array to output the whole combination.
+        /// </summary>
+        /// <param name="optionals">The input array</param>
+        /// <param name="start">The current start index.</param>
+        /// <param name="end">The end index.</param>
+        /// <param name="current">The current iteration stack</param>
+        /// <param name="full">The whole combination.</param>
+        private static void Traveral(IEdmOptionalParameter[] optionals, int start, int end,
+            Stack<IEdmOptionalParameter> current, IList<IEdmOptionalParameter[]> full)
+        {
+            if (start == end)
+            {
+                full.Add(current.Reverse().ToArray());
+                return;
+            }
+
+            Traveral(optionals, start + 1, end, current, full);
+
+            current.Push(optionals[start]);
+
+            Traveral(optionals, start + 1, end, current, full);
+
+            current.Pop();
+        }
+
+        /// <summary>
+        /// Splits the parameters into parts, one is the required parameters, the other is optional parameters.
+        /// </summary>
+        /// <param name="edmFunction">The input function.</param>
+        /// <returns></returns>
+        private static (IList<IEdmOperationParameter>, IList<IEdmOptionalParameter>) SplitParameters(IEdmFunction edmFunction)
+        {
+            IList<IEdmOperationParameter> fixes = new List<IEdmOperationParameter>();
+            IList<IEdmOptionalParameter> optionals = new List<IEdmOptionalParameter>();
+            int skip = edmFunction.IsBound ? 1 : 0;
+            foreach (var parameter in edmFunction.Parameters.Skip(skip))
+            {
+                IEdmOptionalParameter optional = parameter as IEdmOptionalParameter;
+                if (optional != null)
+                {
+                    optionals.Add(optional);
+                }
+                else
+                {
+                    fixes.Add(parameter);
+                }
+            }
+
+            return (fixes, optionals);
+        }
+
+        /// <summary>
+        /// Combinates the next template to the existing templates.
+        /// </summary>
+        /// <param name="templates">The existing templates.</param>
+        /// <param name="nextTemplate">The nexte template.</param>
+        /// <returns>The templates.</returns>
+        private static IList<StringBuilder> CombinateTemplate(IList<StringBuilder> templates, string nextTemplate)
+        {
+            Contract.Assert(templates != null);
+            Contract.Assert(nextTemplate != null);
+
+            foreach (StringBuilder sb in templates)
+            {
+                sb.Append(nextTemplate);
+            }
+
+            return templates;
+        }
+
+        /// <summary>
+        /// Combinates the next templates with existing templates.
+        /// </summary>
+        /// <param name="templates">The existing templates.</param>
+        /// <param name="nextTemplates">The nexte templates.</param>
+        /// <returns>The new templates.</returns>
+        private static IList<StringBuilder> CombinateTemplates(IList<StringBuilder> templates, params string[] nextTemplates)
+        {
+            return CombinateTemplates(templates, new List<string>(nextTemplates));
+        }
+
+        /// <summary>
+        /// Combinates the next templates with existing templates.
+        /// </summary>
+        /// <param name="templates">The existing templates.</param>
+        /// <param name="nextTemplates">The nexte templates.</param>
+        /// <returns>The new templates.</returns>
+        private static IList<StringBuilder> CombinateTemplates(IList<StringBuilder> templates, IList<string> nextTemplates)
+        {
+            Contract.Assert(templates != null);
+
+            IList<StringBuilder> newList = new List<StringBuilder>(templates.Count * nextTemplates.Count);
+
+            foreach (StringBuilder sb in templates)
+            {
+                string oldTemplate = sb.ToString();
+                foreach (string newTemplate in nextTemplates)
+                {
+                    newList.Add(new StringBuilder(oldTemplate).Append(newTemplate));
+                }
+            }
+
+            return newList;
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.OData.Tests/Routing/Template/ODataPathTemplateExtensionsTests.cs
@@ -0,0 +1,242 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.OData.Routing.Template;
+using Microsoft.AspNetCore.OData.Tests.Commons;
+using Microsoft.OData.Edm;
+using Xunit;
+
+namespace Microsoft.AspNetCore.OData.Tests.Routing.Template
+{
+    public class ODataPathTemplateExtensionsTests
+    {
+        private static IEdmTypeReference IntType = EdmCoreModel.Instance.GetPrimitive(EdmPrimitiveTypeKind.Int32, isNullable: false);
+
+        [Fact]
+        public void GetAllTemplatesWorksForBasicPath()
+        {
+            // Arrange
+            EdmEntityType customer = new EdmEntityType("NS", "Customer");
+            customer.AddKeys(customer.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+            EdmEntityContainer container = new EdmEntityContainer("NS", "Default");
+            var entitySet = container.AddEntitySet("Customers", customer);
+
+            ODataPathTemplate template = new ODataPathTemplate(
+                new EntitySetSegmentTemplate(entitySet),
+                new KeySegmentTemplate(customer, entitySet));
+
+            // Act
+            IEnumerable<string> actual = template.GetAllTemplates();
+
+            // Assert
+            Assert.Equal(2, actual.Count());
+            Assert.Equal(new[] { "Customers({key})", "Customers/{key}" }, actual);
+        }
+
+        [Fact]
+        public void GetAllTemplatesWorksForPathWithTypeCastAndFunction()
+        {
+            // Arrange
+            EdmEntityType customer = new EdmEntityType("NS", "Customer");
+            customer.AddKeys(customer.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+
+            // VipCustomer
+            EdmEntityType vipCustomer = new EdmEntityType("NS", "VipCustomer", customer);
+
+            EdmEntityContainer container = new EdmEntityContainer("NS", "Default");
+            var entitySet = container.AddEntitySet("Customers", customer);
+
+            // function with optional parameters
+            EdmFunction getSalaray = new EdmFunction("NS", "GetWholeSalary", IntType, isBound: true, entitySetPathExpression: null, isComposable: false);
+            getSalaray.AddParameter("entityset", new EdmEntityTypeReference(vipCustomer, false));
+            getSalaray.AddParameter("salary", IntType);
+            getSalaray.AddOptionalParameter("minSalary", IntType);
+            getSalaray.AddOptionalParameter("maxSalary", IntType, "129");
+
+            ODataPathTemplate template = new ODataPathTemplate(
+                new EntitySetSegmentTemplate(entitySet),
+                new KeySegmentTemplate(customer, entitySet),
+                new CastSegmentTemplate(vipCustomer, customer, entitySet),
+                new FunctionSegmentTemplate(getSalaray, null));
+
+            // Act
+            IEnumerable<string> actual = template.GetAllTemplates();
+
+            Assert.Equal(16, actual.Count());
+            Assert.Equal(new[]
+            {
+                "Customers({key})/NS.VipCustomer/NS.GetWholeSalary(salary={salary})",
+                "Customers({key})/NS.VipCustomer/NS.GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                "Customers({key})/NS.VipCustomer/NS.GetWholeSalary(salary={salary},minSalary={minSalary})",
+                "Customers({key})/NS.VipCustomer/NS.GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+
+                "Customers({key})/NS.VipCustomer/GetWholeSalary(salary={salary})",
+                "Customers({key})/NS.VipCustomer/GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                "Customers({key})/NS.VipCustomer/GetWholeSalary(salary={salary},minSalary={minSalary})",
+                "Customers({key})/NS.VipCustomer/GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+
+                "Customers/{key}/NS.VipCustomer/NS.GetWholeSalary(salary={salary})",
+                "Customers/{key}/NS.VipCustomer/NS.GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                "Customers/{key}/NS.VipCustomer/NS.GetWholeSalary(salary={salary},minSalary={minSalary})",
+                "Customers/{key}/NS.VipCustomer/NS.GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+
+                "Customers/{key}/NS.VipCustomer/GetWholeSalary(salary={salary})",
+                "Customers/{key}/NS.VipCustomer/GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                "Customers/{key}/NS.VipCustomer/GetWholeSalary(salary={salary},minSalary={minSalary})",
+                "Customers/{key}/NS.VipCustomer/GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+            }, actual);
+        }
+
+
+        #region GenerateFunctionTemplates
+
+        public static TheoryDataSet<bool, string[]> FunctionWithoutParametersData
+        {
+            get
+            {
+                return new TheoryDataSet<bool, string[]>()
+                {
+                    { true, new[]
+                        {
+                            "NS.GetWholeSalary()",
+                            "GetWholeSalary()"
+                        }
+                    },
+                    { false, new[] { "GetWholeSalary()" } },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(FunctionWithoutParametersData))]
+        public void GenerateFunctionTemplatesWorksForEdmFunctionWithoutParameters(bool bound, string[] expects)
+        {
+            // Arrange
+            EdmFunction getSalaray = new EdmFunction("NS", "GetWholeSalary", IntType, isBound: bound, entitySetPathExpression: null, isComposable: false);
+            if (bound)
+            {
+                EdmEntityType customer = new EdmEntityType("NS", "Customer");
+                getSalaray.AddParameter("entityset", new EdmCollectionTypeReference(new EdmCollectionType(new EdmEntityTypeReference(customer, false))));
+            }
+
+            // Act
+            IList<string> items = getSalaray.GenerateFunctionTemplates();
+
+            // Assert
+            Assert.Equal(expects.Length, items.Count);
+            Assert.Equal(expects, items);
+        }
+
+        public static TheoryDataSet<bool, string[]> FunctionWithoutOptionalParametersData
+        {
+            get
+            {
+                return new TheoryDataSet<bool, string[]>()
+                {
+                    { true, new[]
+                        {
+                            "NS.GetWholeSalary(minSalary={minSalary},maxSalary={maxSalary})",
+                            "GetWholeSalary(minSalary={minSalary},maxSalary={maxSalary})"
+                        }
+                    },
+                    { false, new[] { "GetWholeSalary(minSalary={minSalary},maxSalary={maxSalary})" } },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(FunctionWithoutOptionalParametersData))]
+        public void GenerateFunctionTemplatesWorksForEdmFunctionWithoutOptionalParameters(bool bound, string[] expects)
+        {
+            // Arrange
+            EdmFunction getSalaray = new EdmFunction("NS", "GetWholeSalary", IntType, isBound: bound, entitySetPathExpression: null, isComposable: false);
+            if (bound)
+            {
+                EdmEntityType customer = new EdmEntityType("NS", "Customer");
+                customer.AddKeys(customer.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+                getSalaray.AddParameter("entityset", new EdmCollectionTypeReference(new EdmCollectionType(new EdmEntityTypeReference(customer, false))));
+            }
+
+            getSalaray.AddParameter("minSalary", IntType);
+            getSalaray.AddParameter("maxSalary", IntType);
+
+            // Act
+            IList<string> items = getSalaray.GenerateFunctionTemplates();
+
+            // Assert
+            Assert.Equal(expects.Length, items.Count);
+            Assert.Equal(expects, items);
+        }
+
+        public static TheoryDataSet<bool, string[]> FunctionWithOptionalParametersData
+        {
+            get
+            {
+                return new TheoryDataSet<bool, string[]>()
+                {
+                    { true, new[]
+                        {
+                            "NS.GetWholeSalary(salary={salary})",
+                            "NS.GetWholeSalary(salary={salary},aveSalary={aveSalary})",
+                            "NS.GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                            "NS.GetWholeSalary(salary={salary},maxSalary={maxSalary},aveSalary={aveSalary})",
+                            "NS.GetWholeSalary(salary={salary},minSalary={minSalary})",
+                            "NS.GetWholeSalary(salary={salary},minSalary={minSalary},aveSalary={aveSalary})",
+                            "NS.GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+                            "NS.GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary},aveSalary={aveSalary})",
+
+                            "GetWholeSalary(salary={salary})",
+                            "GetWholeSalary(salary={salary},aveSalary={aveSalary})",
+                            "GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                            "GetWholeSalary(salary={salary},maxSalary={maxSalary},aveSalary={aveSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary},aveSalary={aveSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary},aveSalary={aveSalary})",
+                        }
+                    },
+                    { false, new[]
+                        {
+                            "GetWholeSalary(salary={salary})",
+                            "GetWholeSalary(salary={salary},aveSalary={aveSalary})",
+                            "GetWholeSalary(salary={salary},maxSalary={maxSalary})",
+                            "GetWholeSalary(salary={salary},maxSalary={maxSalary},aveSalary={aveSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary},aveSalary={aveSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary})",
+                            "GetWholeSalary(salary={salary},minSalary={minSalary},maxSalary={maxSalary},aveSalary={aveSalary})",
+                        }
+                    }
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(FunctionWithOptionalParametersData))]
+        public void GenerateFunctionTemplatesWorksForEdmFunctionWithOptionalParameters(bool bound, string[] expects)
+        {
+            // Arrange
+            EdmFunction getSalaray = new EdmFunction("NS", "GetWholeSalary", IntType, isBound: bound, entitySetPathExpression: null, isComposable: false);
+            if (bound)
+            {
+                EdmEntityType customer = new EdmEntityType("NS", "Customer");
+                getSalaray.AddParameter("entityset", new EdmCollectionTypeReference(new EdmCollectionType(new EdmEntityTypeReference(customer, false))));
+            }
+
+            getSalaray.AddParameter("salary", IntType);
+            getSalaray.AddOptionalParameter("minSalary", IntType);
+            getSalaray.AddOptionalParameter("maxSalary", IntType);
+            getSalaray.AddOptionalParameter("aveSalary", IntType, "129");
+
+            // Act
+            var items = getSalaray.GenerateFunctionTemplates();
+
+            // Assert
+            Assert.Equal(expects.Length, items.Count);
+            Assert.Equal(expects, items);
+        }
+        #endregion
+    }
+}
