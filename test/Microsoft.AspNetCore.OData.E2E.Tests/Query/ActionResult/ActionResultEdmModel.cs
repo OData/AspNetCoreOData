40a29c918bd7f7d8834c4bc8435cf37a6a9812a8
Ramjot Singh <ramjsing@microsoft.com>
2020-12-14 02:25:05 -0800

    Fixing usings

--- test/Microsoft.AspNetCore.OData.E2E.Tests/Query/ActionResult/ActionResultEdmModel.cs
+++ test/Microsoft.AspNetCore.OData.E2E.Tests/Query/ActionResult/ActionResultEdmModel.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
 namespace Microsoft.AspNetCore.OData.E2E.Tests.Query.ActionResult
 {
     public class ActionResultEdmModel
