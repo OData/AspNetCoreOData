5d6502331e5ca02f4ac99b38afa5333feb3eba34
Adalberto Rua <adalbertor@solin.com.co>
2020-12-16 16:51:26 -0800

    Custom implementation for JSON serialization of AggregationWrapper

---
+++ src/Microsoft.AspNetCore.OData/Query/Wrapper/AggregationTypeWrapperConverter.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Microsoft.AspNetCore.OData.Query.Wrapper
+{
+    /// <summary>
+    /// Represents a custom <see cref="JsonConverter"/> to serialize <see cref="DynamicTypeWrapper"/> instances to JSON.
+    /// </summary>
+    internal class AggregationTypeWrapperConverter : JsonConverter<AggregationWrapper>
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            if (objectType == null)
+            {
+                throw Error.ArgumentNull("objectType");
+            }
+
+            return objectType.IsAssignableFrom(typeof(AggregationWrapper));
+        }
+
+        public override AggregationWrapper Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            Contract.Assert(false, "DynamicTypeWrapper is internal and should never be deserialized into.");
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, [DisallowNull] AggregationWrapper value, JsonSerializerOptions options)
+        {
+            if (value != null)
+            {
+                writer.WriteStringValue(JsonSerializer.Serialize(value.Values));
+            }
+        }
+    }
+}
--- src/Microsoft.AspNetCore.OData/Query/Wrapper/AggregationWrapper.cs
+++ src/Microsoft.AspNetCore.OData/Query/Wrapper/AggregationWrapper.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace Microsoft.AspNetCore.OData.Query.Wrapper
 {
+    [JsonConverter(typeof(AggregationTypeWrapperConverter))]
     internal class AggregationWrapper : GroupByWrapper
     {
     }
