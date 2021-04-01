8ab2b483401e7bad5e7df64f088865fd04e72bac
Sam Xu <saxu@microsoft.com>
2021-04-01 15:01:48 -0700

    Add SingleResult E2E test cases

---
+++ test/Microsoft.AspNetCore.OData.E2E.Tests/SingleResult/SingleResultController.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace Microsoft.AspNetCore.OData.E2E.Tests.SingleResultTest
+{
+    public class CustomersController : ODataController
+    {
+        private readonly SingleResultContext _db = new SingleResultContext();
+
+        [EnableQuery]
+        public SingleResult<Customer> Get(int key)
+        {
+            ResetDataSource();
+            var db = new SingleResultContext();
+            return SingleResult.Create<Customer>(db.Customers.Where(c => c.Id == key));
+        }
+
+        public void Generate()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                var customer = new Customer
+                {
+                    Name = $"name_{i}",
+                    Orders = new List<Order>
+                    {
+                        new Order
+                        {
+                            Title = $"title_{i}",
+                        }
+                    }
+                };
+
+                _db.Customers.Add(customer);
+            }
+
+            _db.SaveChanges();
+        }
+
+        private void ResetDataSource()
+        {
+            _db.Database.EnsureCreated();
+            if (!_db.Customers.Any())
+            {
+                Generate();
+            }
+        }
+    }
+}
