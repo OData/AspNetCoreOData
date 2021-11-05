b26608866ba2ea5304c3d42d2252206e214723d1
David Robinson <david.robinson@live.com>
2021-11-05 14:04:53 -0700

    Unmapped properties are in Delta<T> #347

---
+++ test/Microsoft.AspNetCore.OData.Tests/Models/NewCustomerUnmapped.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Microsoft.AspNetCore.OData.Tests.Models
+{
+    public class NewCustomerUnmapped
+    {
+        [Key]
+        public int Id { get; set; }
+        [IgnoreDataMember]
+        public string Name { get; set; }
+        [NotMapped]
+        public int Age { get; set; }
+
+        [DataMember]
+        [IgnoreDataMember]
+        public string Street { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        public string State { get; set; }
+    }
+}
