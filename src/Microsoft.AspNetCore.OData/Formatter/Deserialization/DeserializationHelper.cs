a273006edde2e639c16fda955944835eea358dee
Sam Xu <saxu@microsoft.com>
2022-12-01 12:11:05 -0800

    Enable the nested delta collection deep update (#747)
    * Enable the nested delta collection deep update
    
    * Address the comments

--- src/Microsoft.AspNetCore.OData/Formatter/Deserialization/DeserializationHelper.cs
+++ src/Microsoft.AspNetCore.OData/Formatter/Deserialization/DeserializationHelper.cs
@@ -115,6 +115,13 @@ namespace Microsoft.AspNetCore.OData.Formatter.Deserialization
         {
             if (value != null)
             {
+                // If the setting value is a delta set, we don't need to create a new collection, just use it.
+                if (value is IDeltaSet set)
+                {
+                    SetProperty(resource, propertyName, set);
+                    return;
+                }
+
                 IEnumerable collection = value as IEnumerable;
                 Contract.Assert(collection != null,
                     "SetCollectionProperty is always passed the result of ODataFeedDeserializer or ODataCollectionDeserializer");
