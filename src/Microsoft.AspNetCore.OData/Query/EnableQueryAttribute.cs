4de92f52a346606a447ec4df96c5f3cd05642f50
Kennedy Kang'ethe <kemunga@microsoft.com>
2022-12-20 10:41:04 +0300

    Enable classes derived from EnableQueryAttribute to unit test OnActionExecuted in isolation (#776)

--- src/Microsoft.AspNetCore.OData/Query/EnableQueryAttribute.cs
+++ src/Microsoft.AspNetCore.OData/Query/EnableQueryAttribute.cs
@@ -542,7 +542,7 @@ namespace Microsoft.AspNetCore.OData.Query
         {
             RequestQueryData requestQueryData = request.HttpContext.Items[nameof(RequestQueryData)] as RequestQueryData;
 
-            if (requestQueryData.QueryValidationRunBeforeActionExecution)
+            if (requestQueryData != null && requestQueryData.QueryValidationRunBeforeActionExecution)
             {
                 return requestQueryData.ProcessedQueryOptions;
             }
