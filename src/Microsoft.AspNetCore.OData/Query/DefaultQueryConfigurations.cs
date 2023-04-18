026c647e214b2a0ea2017e83ebc7c7773477bcbb
Sam Xu <saxu@microsoft.com>
2023-04-18 14:20:41 -0700

    Re-add the removed 'QuerySettings' to avoid breaking changes.
    Co-Author: Chris Martinez

--- src/Microsoft.AspNetCore.OData/Query/DefaultQueryConfigurations.cs
+++ src/Microsoft.AspNetCore.OData/Query/DefaultQueryConfigurations.cs
@@ -5,63 +5,16 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
+using Microsoft.OData.ModelBuilder.Config;
+
 namespace Microsoft.AspNetCore.OData.Query
 {
     /// <summary>
     /// This class describes the default configurations to use during query composition.
     /// </summary>
-    public class DefaultQueryConfigurations
+    public class DefaultQueryConfigurations : DefaultQuerySettings
     {
-        private int? _maxTop = 0;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether navigation property can be expanded.
-        /// </summary>
-        public bool EnableExpand { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether property can be selected.
-        /// </summary>
-        public bool EnableSelect { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether entity set and property can apply $count.
-        /// </summary>
-        public bool EnableCount { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether property can apply $orderby.
-        /// </summary>
-        public bool EnableOrderBy { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether property can apply $filter.
-        /// </summary>
-        public bool EnableFilter { get; set; }
-
-        /// <summary>
-        /// Gets or sets the max value of $top that a client can request.
-        /// </summary>
-        /// <value>
-        /// The max value of $top that a client can request, or <c>null</c> if there is no limit.
-        /// </value>
-        public int? MaxTop
-        {
-            get => _maxTop;
-            set
-            {
-                if (value.HasValue && value < 0)
-                {
-                    throw Error.ArgumentMustBeGreaterThanOrEqualTo("value", value, 0);
-                }
-
-                _maxTop = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether the service will use skiptoken or not.
-        /// </summary>
-        public bool EnableSkipToken { get; set; }
+        // We will add other query settings, for example, $compute, $search here
+        // In the next major release, we should remove the inheritance from 'DefaultQuerySettings'.
     }
 }
