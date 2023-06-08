9e073d1cc938591f7115b792a6e12c57171e4e11
Lisi Case <49213376+lisicase@users.noreply.github.com>
2023-06-08 11:19:09 -0700

    started creating ApplyQueryContext class

---
+++ QueryBuilder/Query/ApplyQueryContext.cs
@@ -0,0 +1,23 @@
+﻿using QueryBuilder.Abstracts;
+
+namespace QueryBuilder.Query
+{
+    /// <summary>
+    /// This defines some context information used to apply queries.
+    /// </summary>
+    public class ApplyQueryContext
+    {
+        public ApplyQueryContext(IQueryable query, IODataFeature odataFeature, ODataQuerySettings querySettings)
+        {
+            Query = query;
+            ODataFeature = odataFeature;
+            QuerySettings = querySettings; // > Context.GetODataQuerySettings();
+        }
+
+        public IQueryable Query { get; internal set; }
+
+        public IODataFeature ODataFeature { get; internal set; }
+
+        public ODataQuerySettings QuerySettings { get; internal set; }
+    }
+}
