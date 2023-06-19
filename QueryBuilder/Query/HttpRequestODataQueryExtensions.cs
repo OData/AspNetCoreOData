7f99fc29fc4e640e0f88ffac67354a07487d4d12
Lisi Case <49213376+lisicase@users.noreply.github.com>
2023-06-19 09:21:13 -0700

    Added ODataQueryOptions<T>

---
+++ QueryBuilder/Query/HttpRequestODataQueryExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Linq;
+//using Microsoft.AspNetCore.Http;
+using QueryBuilder.Abstracts;
+using QueryBuilder.Edm;
+//using QueryBuilder.Extensions;
+using QueryBuilder.Routing;
+//using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using QueryBuilder.Edm;
+using QueryBuilder.Query;
+using QueryBuilder;
+
+namespace QueryBuilder.Query
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class HttpRequestODataQueryExtensions
+    {
+        /// <summary>
+        /// Gets the OData <see cref="ETag"/> from the given request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="entityTagHeaderValue">The entity tag header value.</param>
+        /// <returns>The parsed <see cref="ETag"/>.</returns>
+        [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling", Justification = "Relies on many ODataLib classes.")]
+        public static ETag GetETag(EntityTagHeaderValue entityTagHeaderValue, IETagHandler etagHandler, IEdmModel model, IEdmNavigationSource source)
+        {
+            if (entityTagHeaderValue != null && etagHandler != null && model != null && source != null)
+            {
+                if (entityTagHeaderValue.Equals(EntityTagHeaderValue.Any))
+                {
+                    return new ETag { IsAny = true };
+                }
+
+                // get the etag handler, and parse the etag
+                //IETagHandler etagHandler = request.GetRouteServices().GetRequiredService<IETagHandler>();
+                IDictionary<string, object> properties = etagHandler.ParseETag(entityTagHeaderValue) ?? new Dictionary<string, object>();
+                IList<object> parsedETagValues = properties.Select(property => property.Value).ToList();
+
+                // get property names from request
+                //ODataPath odataPath = request.ODataFeature().Path;
+                //IEdmModel model = request.GetModel();
+                //IEdmNavigationSource source = odataPath.GetNavigationSource();
+                if (model != null && source != null)
+                {
+                    IList<IEdmStructuralProperty> concurrencyProperties = model.GetConcurrencyProperties(source).ToList();
+                    IList<string> concurrencyPropertyNames = concurrencyProperties.OrderBy(c => c.Name).Select(c => c.Name).ToList();
+                    ETag etag = new ETag();
+
+                    if (parsedETagValues.Count != concurrencyPropertyNames.Count)
+                    {
+                        etag.IsWellFormed = false;
+                    }
+
+                    IEnumerable<KeyValuePair<string, object>> nameValues = concurrencyPropertyNames.Zip(
+                        parsedETagValues,
+                        (name, value) => new KeyValuePair<string, object>(name, value));
+                    foreach (var nameValue in nameValues)
+                    {
+                        IEdmStructuralProperty property = concurrencyProperties.SingleOrDefault(e => e.Name == nameValue.Key);
+                        Contract.Assert(property != null);
+
+                        Type clrType = model.GetClrType(property.Type);
+                        Contract.Assert(clrType != null);
+
+                        if (nameValue.Value != null)
+                        {
+                            Type valueType = nameValue.Value.GetType();
+                            etag[nameValue.Key] = valueType != clrType
+                                ? Convert.ChangeType(nameValue.Value, clrType, CultureInfo.InvariantCulture)
+                                : nameValue.Value;
+                        }
+                        else
+                        {
+                            etag[nameValue.Key] = nameValue.Value;
+                        }
+                    }
+
+                    return etag;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ETag{TEntity}"/> from the given request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="entityTagHeaderValue">The entity tag header value.</param>
+        /// <returns>The parsed <see cref="ETag{TEntity}"/>.</returns>
+        public static ETag<TEntity> GetETag<TEntity>(EntityTagHeaderValue entityTagHeaderValue, IETagHandler etagHandler, IEdmModel model, IEdmNavigationSource source)
+        {
+            ETag etag = GetETag(entityTagHeaderValue, etagHandler, model, source);
+            return etag != null
+                ? new ETag<TEntity>
+                {
+                    ConcurrencyProperties = etag.ConcurrencyProperties,
+                    IsWellFormed = etag.IsWellFormed,
+                    IsAny = etag.IsAny,
+                }
+                : null;
+        }
+    }
+}
--- QueryBuilder/Query/ODataQueryOptionsOfT.cs
+++ QueryBuilder/Query/ODataQueryOptionsOfT.cs
@@ -1,105 +1,118 @@
-﻿//using System.Linq;
-////using Microsoft.AspNetCore.Http;
-//using QueryBuilder.Common;
-//using Microsoft.Net.Http.Headers;
-//using Microsoft.OData.Edm;
+﻿using System.Linq;
+//using Microsoft.AspNetCore.Http;
+using QueryBuilder.Common;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OData.Edm;
+using Microsoft.Extensions.Primitives;
+using QueryBuilder.Abstracts;
+using Microsoft.OData.ModelBuilder;
+using QueryBuilder.Query.Expressions;
 
-//namespace QueryBuilder.Query
-//{
-//    /// <summary>
-//    /// This defines a composite OData query options that can be used to perform query composition.
-//    /// Currently this only supports $filter, $orderby, $top, $skip.
-//    /// </summary>
-//    // TODO: Fix attribute compilation below:
-//    //[ODataQueryParameterBinding]
-//    public class ODataQueryOptions<TEntity> : ODataQueryOptions
-//    {
-//        /// <summary>
-//        /// Initializes a new instance of the <see cref="ODataQueryOptions"/> class based on the incoming request and some metadata information from
-//        /// the <see cref="ODataQueryContext"/>.
-//        /// </summary>
-//        /// <param name="context">The <see cref="ODataQueryContext"/> which contains the <see cref="IEdmModel"/> and some type information</param>
-//        /// <param name="request">The incoming request message</param>
-//        /// <remarks>This signature uses types that are AspNetCore-specific.</remarks>
-//        public ODataQueryOptions(ODataQueryContext context, HttpRequest request)
-//            : base(context, request)
-//        {
-//            if (Context.ElementClrType == null)
-//            {
-//                throw Error.Argument("context", SRResources.ElementClrTypeNull, typeof(ODataQueryContext).Name);
-//            }
+namespace QueryBuilder.Query
+{
+    /// <summary>
+    /// This defines a composite OData query options that can be used to perform query composition.
+    /// Currently this only supports $filter, $orderby, $top, $skip.
+    /// </summary>
+    // TODO: Fix attribute compilation below:
+    //[ODataQueryParameterBinding]
+    public class ODataQueryOptions<TEntity> : ODataQueryOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ODataQueryOptions"/> class based on the incoming request and some metadata information from
+        /// the <see cref="ODataQueryContext2"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="ODataQueryContext2"/> which contains the <see cref="IEdmModel"/> and some type information</param>
+        /// <param name="request">The incoming request message</param>
+        /// <remarks>This signature uses types that are AspNetCore-specific.</remarks>
+        public ODataQueryOptions(ODataQueryContext2 context, string requestUri, IQueryCollection requestQueryCollection)
+            : base(context, requestUri, requestQueryCollection)
+        {
+            if (Context.ElementClrType == null)
+            {
+                throw Error.Argument("context", SRResources.ElementClrTypeNull, typeof(ODataQueryContext2).Name);
+            }
 
-//            if (context.ElementClrType != typeof(TEntity))
-//            {
-//                throw Error.Argument("context", SRResources.EntityTypeMismatch, context.ElementClrType.FullName, typeof(TEntity).FullName);
-//            }
-//        }
+            if (context.ElementClrType != typeof(TEntity))
+            {
+                throw Error.Argument("context", SRResources.EntityTypeMismatch, context.ElementClrType.FullName, typeof(TEntity).FullName);
+            }
+        }
 
-//        /// <summary>
-//        /// Gets the <see cref="ETag{TEntity}"/> from IfMatch header.
-//        /// </summary>
-//        public new ETag<TEntity> IfMatch
-//        {
-//            get
-//            {
-//                return base.IfMatch as ETag<TEntity>;
-//            }
-//        }
+        /// <summary>
+        /// Gets the <see cref="ETag{TEntity}"/> from IfMatch header.
+        /// </summary>
+        public new ETag<TEntity> IfMatch(StringValues ifMatchValues, IETagHandler etagHandler)
+        {
+            return base.IfMatch(ifMatchValues, etagHandler) as ETag<TEntity>;
+        }
 
-//        /// <summary>
-//        /// Gets the <see cref="ETag{TEntity}"/> from IfNoneMatch header.
-//        /// </summary>
-//        public new ETag<TEntity> IfNoneMatch
-//        {
-//            get
-//            {
-//                return base.IfNoneMatch as ETag<TEntity>;
-//            }
-//        }
+        /// <summary>
+        /// Gets the <see cref="ETag{TEntity}"/> from IfNoneMatch header.
+        /// </summary>
+        public new ETag<TEntity> IfNoneMatch(StringValues ifNoneMatchValues, IETagHandler etagHandler)
+        {
+            return base.IfNoneMatch(ifNoneMatchValues, etagHandler) as ETag<TEntity>;
+        }
 
-//        /// <summary>
-//        /// Gets the EntityTagHeaderValue ETag>.
-//        /// </summary>
-//        /// <remarks>This signature uses types that are AspNetCore-specific.</remarks>
-//        internal override ETag GetETag(EntityTagHeaderValue etagHeaderValue)
-//        {
-//            return Request.GetETag<TEntity>(etagHeaderValue);
-//        }
+        /// <summary>
+        /// Gets the EntityTagHeaderValue ETag>.
+        /// </summary>
+        /// <remarks>This signature uses types that are AspNetCore-specific.</remarks>
+        internal override ETag GetETag(EntityTagHeaderValue entityTagHeaderValue, IETagHandler etagHandler)
+        {
+            return HttpRequestODataQueryExtensions.GetETag<TEntity>(entityTagHeaderValue, etagHandler, (IEdmModel) null, (IEdmNavigationSource) null);
+        }
 
-//        /// <summary>
-//        /// Apply the individual query to the given IQueryable in the right order.
-//        /// </summary>
-//        /// <param name="query">The original <see cref="IQueryable"/>.</param>
-//        /// <returns>The new <see cref="IQueryable"/> after the query has been applied to.</returns>
-//        public override IQueryable ApplyTo(IQueryable query)
-//        {
-//            ValidateQuery(query);
-//            return base.ApplyTo(query);
-//        }
+        /// <summary>
+        /// Gets the EntityTagHeaderValue ETag>.
+        /// </summary>
+        /// <remarks>This signature uses types that are AspNetCore-specific.</remarks>
+        internal ETag GetETag(EntityTagHeaderValue entityTagHeaderValue, IETagHandler etagHandler, IEdmModel model, IEdmNavigationSource source)
+        {
+            return HttpRequestODataQueryExtensions.GetETag<TEntity>(entityTagHeaderValue, etagHandler, model, source);
+        }
 
-//        /// <summary>
-//        /// Apply the individual query to the given IQueryable in the right order.
-//        /// </summary>
-//        /// <param name="query">The original <see cref="IQueryable"/>.</param>
-//        /// <param name="querySettings">The settings to use in query composition.</param>
-//        /// <returns>The new <see cref="IQueryable"/> after the query has been applied to.</returns>
-//        public override IQueryable ApplyTo(IQueryable query, ODataQuerySettings querySettings)
-//        {
-//            ValidateQuery(query);
-//            return base.ApplyTo(query, querySettings);
-//        }
+        /// <summary>
+        /// Apply the individual query to the given IQueryable in the right order.
+        /// </summary>
+        /// <param name="query">The original <see cref="IQueryable"/>.</param>
+        /// <returns>The new <see cref="IQueryable"/> after the query has been applied to.</returns>
+        public override IQueryable ApplyTo(IQueryable query, ODataQuerySettings querySettings, AllowedQueryOptions ignoreQueryOptions, ODataFeature odataFeature, IAssemblyResolver assembliesResolver,
+                                          IQueryCollection requestQueryCollection, string encodedUrl, int preferredPageSize = -1,
+                                          IOrderByBinder orderByBinder = null, ISelectExpandBinder selectExpandBinder = null, IFilterBinder filterBinder = null)
+        {
+            ValidateQuery(query);
+            return base.ApplyTo(query, querySettings, ignoreQueryOptions, odataFeature, assembliesResolver, requestQueryCollection,
+                                encodedUrl, preferredPageSize, orderByBinder, selectExpandBinder, filterBinder);
+        }
 
-//        private static void ValidateQuery(IQueryable query)
-//        {
-//            if (query == null)
-//            {
-//                throw Error.ArgumentNull("query");
-//            }
+        /// <summary>
+        /// Apply the individual query to the given IQueryable in the right order.
+        /// </summary>
+        /// <param name="query">The original <see cref="IQueryable"/>.</param>
+        /// <param name="querySettings">The settings to use in query composition.</param>
+        /// <returns>The new <see cref="IQueryable"/> after the query has been applied to.</returns>
+        public override IQueryable ApplyTo(IQueryable query, ODataQuerySettings querySettings, ODataFeature odataFeature, IAssemblyResolver assembliesResolver,
+                                          IQueryCollection requestQueryCollection, string encodedUrl, int preferredPageSize = -1,
+                                          IOrderByBinder orderByBinder = null, ISelectExpandBinder selectExpandBinder = null, IFilterBinder filterBinder = null)
+        {
+            ValidateQuery(query);
+            return base.ApplyTo(query, querySettings, odataFeature, assembliesResolver, requestQueryCollection,
+                                encodedUrl, preferredPageSize, orderByBinder, selectExpandBinder, filterBinder);
+        }
 
-//            if (!TypeHelper.IsTypeAssignableFrom(typeof(TEntity), query.ElementType))
-//            {
-//                throw Error.Argument("query", SRResources.CannotApplyODataQueryOptionsOfT, typeof(ODataQueryOptions).Name, typeof(TEntity).FullName, typeof(IQueryable).Name, query.ElementType.FullName);
-//            }
-//        }
-//    }
-//}
+        private static void ValidateQuery(IQueryable query)
+        {
+            if (query == null)
+            {
+                throw Error.ArgumentNull("query");
+            }
+
+            if (!TypeHelper.IsTypeAssignableFrom(typeof(TEntity), query.ElementType))
+            {
+                throw Error.Argument("query", SRResources.CannotApplyODataQueryOptionsOfT, typeof(ODataQueryOptions).Name, typeof(TEntity).FullName, typeof(IQueryable).Name, query.ElementType.FullName);
+            }
+        }
+    }
+}
