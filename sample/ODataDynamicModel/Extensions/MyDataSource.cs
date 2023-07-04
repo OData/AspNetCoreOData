45b140b2f1c71fe15ff3a31d28272d295eff8fee
Elizabeth Okerio <elizaokerio@gmail.com>
2023-07-04 07:25:30 +0300

    update dynamic sample (#975)

--- sample/ODataDynamicModel/Extensions/MyDataSource.cs
+++ sample/ODataDynamicModel/Extensions/MyDataSource.cs
@@ -37,8 +37,7 @@ namespace ODataDynamicModel.Extensions
             detailInfo.AddKeys(detailInfo.AddStructuralProperty("ID", EdmPrimitiveTypeKind.Int32));
             detailInfo.AddStructuralProperty("Title", EdmPrimitiveTypeKind.String);
             model.AddElement(detailInfo);
-            EdmEntitySet detailInfos = container.AddEntitySet("DetailInfos", product);
-
+            EdmEntitySet detailInfos = container.AddEntitySet("DetailInfos", detailInfo);
             EdmNavigationProperty detailInfoNavProp = product.AddUnidirectionalNavigation(
                 new EdmNavigationPropertyInfo
                 {
@@ -47,8 +46,18 @@ namespace ODataDynamicModel.Extensions
                     Target = detailInfo
                 });
             products.AddNavigationTarget(detailInfoNavProp, detailInfos);
-            _edmModel = model;
-            return _edmModel;
+
+            // Add a contained navigation property
+            product.AddUnidirectionalNavigation(
+            new EdmNavigationPropertyInfo
+            {
+                Name = "ContainedDetailInfo",
+                TargetMultiplicity = EdmMultiplicity.One,
+                Target = detailInfo,
+                ContainsTarget = true
+            });
+             _edmModel = model;
+             return _edmModel;
         }
 
         public void Get(IEdmEntityTypeReference entityType, EdmEntityObjectCollection collection)
@@ -57,12 +66,14 @@ namespace ODataDynamicModel.Extensions
             entity.TrySetPropertyValue("Name", "abc");
             entity.TrySetPropertyValue("ID", 1);
             entity.TrySetPropertyValue("DetailInfo", CreateDetailInfo(88, "abc_detailinfo", entity.ActualEdmType));
+            entity.TrySetPropertyValue("ContainedDetailInfo", CreateContainedDetailInfo(88, "abc_containeddetailinfo", entity.ActualEdmType));
 
             collection.Add(entity);
             entity = new EdmEntityObject(entityType);
             entity.TrySetPropertyValue("Name", "def");
             entity.TrySetPropertyValue("ID", 2);
             entity.TrySetPropertyValue("DetailInfo", CreateDetailInfo(99, "def_detailinfo", entity.ActualEdmType));
+            entity.TrySetPropertyValue("ContainedDetailInfo", CreateContainedDetailInfo(99, "def_containeddetailinfo", entity.ActualEdmType));
 
             collection.Add(entity);
         }
@@ -72,6 +83,7 @@ namespace ODataDynamicModel.Extensions
             entity.TrySetPropertyValue("Name", "abc");
             entity.TrySetPropertyValue("ID", int.Parse(key));
             entity.TrySetPropertyValue("DetailInfo", CreateDetailInfo(88, "abc_detailinfo", entity.ActualEdmType));
+            entity.TrySetPropertyValue("ContainedDetailInfo", CreateContainedDetailInfo(88, "abc_containeddetailinfo", entity.ActualEdmType));
         }
 
         public object GetProperty(string property, EdmEntityObject entity)
@@ -84,6 +96,20 @@ namespace ODataDynamicModel.Extensions
         private IEdmEntityObject CreateDetailInfo(int id, string title, IEdmStructuredType edmType)
         {
             IEdmNavigationProperty navigationProperty = edmType.DeclaredProperties.OfType<EdmNavigationProperty>().FirstOrDefault(e => e.Name == "DetailInfo");
+            if (navigationProperty == null)
+            {
+                return null;
+            }
+
+            EdmEntityObject entity = new EdmEntityObject(navigationProperty.ToEntityType());
+            entity.TrySetPropertyValue("ID", id);
+            entity.TrySetPropertyValue("Title", title);
+            return entity;
+        }
+
+        private IEdmEntityObject CreateContainedDetailInfo(int id, string title, IEdmStructuredType edmType)
+        {
+            IEdmNavigationProperty navigationProperty = edmType.DeclaredProperties.OfType<EdmNavigationProperty>().FirstOrDefault(e => e.Name == "ContainedDetailInfo");
             if (navigationProperty == null)
             {
                 return null;
