251b4bd4fb6c292b9adf83444d93ca4a5bf941ad
Elizabeth Okerio <elizaokerio@gmail.com>
2023-09-26 09:44:11 +0300

    add a profiling sample project

---
+++ ODataPerformanceProfile/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using ODataPerformanceProfile.Models;
+
+namespace ODataPerformanceProfile.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        private ProductsContext _context;
+        private IList<Product> products;
+
+        public ProductsController(ProductsContext context)
+        {
+            /* _context = context;
+
+             if (_context.Products.Count() == 0)
+             {
+
+
+                 foreach (var product in products)
+                 {
+                     _context.Products.Add(product);
+                 }
+
+                 _context.SaveChanges();
+             }*/
+            products = new List<Product>();
+            for (int i = 1; i < 3000; i++)
+            {
+                var prod = new Product()
+                {
+                    Id = i,
+                    Category = "Goods" + i,
+                    Color = Color.Red,
+                    CreatedDate = new DateTimeOffset(2001, 4, 15, 16, 24, 8, TimeSpan.FromHours(-8)),
+                    UpdatedDate = new DateTimeOffset(2011, 2, 15, 16, 24, 8, TimeSpan.FromHours(-8)),
+                    Detail = new ProductDetail { Id = "Id" + i, Info = "Info" + i },
+                    ProductOrders = new List<Order> {
+                        new Order
+                        {
+                            Id = i,
+                            OrderNo = "Order"+i
+                        }
+                    },
+                    ProductSuppliers = new List<Supplier>
+                    {
+                        new Supplier
+                        {
+                            Id = i,
+                            Name = "Supplier"+i,
+                            Description = "SupplierDesc"+i,
+                            SupplierAddress = new Location
+                            {
+                                City = "SupCity"+i,
+                                Address = "SupAddre"+i
+                            }
+                        }
+                    }
+                };
+
+                products.Add(prod);
+            }
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(products);
+        }
+    }
+}
---
+++ ODataPerformanceProfile/EdmModelBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using ODataPerformanceProfile.Models;
+
+namespace ODataPerformanceProfile
+{
+    public static class EdmModelBuilder
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+          //  builder.EntitySet<Supplier>("Suppliers");
+          //  builder.EntitySet<Order>("Orders");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
