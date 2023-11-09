d30c596b15d764f9f2f3447b001904876a124659
Elizabeth Okerio <elizaokerio@gmail.com>
2023-11-09 08:10:44 +0300

    update based on review comments

--- src/Microsoft.AspNetCore.OData/Edm/EdmModelAnnotationExtensions.cs
+++ src/Microsoft.AspNetCore.OData/Edm/EdmModelAnnotationExtensions.cs
@@ -6,7 +6,6 @@
 //------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
