ccfab4849dbdda0ae4f4fff835bfcf798ee224d3
Clément Habinshuti <clhabins@microsoft.com>
2024-01-19 12:17:24 +0300

    Added comments to tests cases

--- test/Microsoft.AspNetCore.OData.Tests/ODataPathExtensionsTests.cs
+++ test/Microsoft.AspNetCore.OData.Tests/ODataPathExtensionsTests.cs
@@ -59,37 +59,60 @@ namespace Microsoft.AspNetCore.OData.Tests
         }
 
         [Theory]
+        // entity set segment should return the entity set
         [InlineData("Customers", "Customers", EdmNavigationSourceKind.EntitySet)]
+        // key segment should return the corresponding entity set
         [InlineData("Customers(1)", "Customers", EdmNavigationSourceKind.EntitySet)]
+        // property segment should return the previous segment's navigation source
         [InlineData("Customers(1)/Name", "Customers", EdmNavigationSourceKind.EntitySet)]
+        // navigation property segment
         [InlineData("Customers(1)/Orders", "Orders", EdmNavigationSourceKind.EntitySet)]
+        // key segment of a navigation property
         [InlineData("Customers(1)/Orders(1)", "Orders", EdmNavigationSourceKind.EntitySet)]
+        // navigation property link segment
         [InlineData("Customers(1)/Orders(1)/$ref", "Orders", EdmNavigationSourceKind.EntitySet)]
+        // property segment of a navigation property
         [InlineData("Customers(1)/Orders(1)/Amount", "Orders", EdmNavigationSourceKind.EntitySet)]
+        // non-contained navigation property without an entity set should return UnknownEntitySet
         [InlineData("MyOrders(1)/NonContainedOrderLines", "NonContainedOrderLines", EdmNavigationSourceKind.UnknownEntitySet)]
         [InlineData("MyOrders(1)/NonContainedOrderLines(1)", "NonContainedOrderLines", EdmNavigationSourceKind.UnknownEntitySet)]
         [InlineData("MyOrders(1)/NonContainedOrderLines(1)/$ref", "NonContainedOrderLines", EdmNavigationSourceKind.UnknownEntitySet)]
+        // contained navigation property should return ContainedEntitySet
         [InlineData("MyOrders(1)/OrderLines", "OrderLines", EdmNavigationSourceKind.ContainedEntitySet)]
         [InlineData("MyOrders(1)/OrderLines(1)", "OrderLines", EdmNavigationSourceKind.ContainedEntitySet)]
         [InlineData("MyOrders(1)/OrderLines(1)/$ref", "OrderLines", EdmNavigationSourceKind.ContainedEntitySet)]
+        // property segment with a complex type, returns navigation source of previous segment
         [InlineData("Customers(1)/Account", "Customers", EdmNavigationSourceKind.EntitySet)]
+        // property segment of a complex type, returns navigation source of previous segment
         [InlineData("Customers(1)/Account/Bank", "Customers", EdmNavigationSourceKind.EntitySet)]
+        // dynamic path segment should return null
         [InlineData("Customers(1)/Account/DynamicProperty", null, EdmNavigationSourceKind.None)]
+        // unbound operation import with an entity set path should return the entity set
         [InlineData("GetTopCustomers()", "Customers", EdmNavigationSourceKind.EntitySet)]
+        // unbound operation import without an entity set path should return null
         [InlineData("GetTotalSalesAmount()", null, EdmNavigationSourceKind.None)]
+        // bound operation without an entity set path should return null
         [InlineData("Customers(1)/NS.IsUpgraded()", null, EdmNavigationSourceKind.None)]
         [InlineData("Customers/NS.UpgradeAll()", null, EdmNavigationSourceKind.None)]
+        // bound operaiton with an entity set path should return the entity set
         [InlineData("Customers/NS.GetTop", "Customers", EdmNavigationSourceKind.EntitySet)]
         [InlineData("Customers/NS.GetBestOrders()", "Orders", EdmNavigationSourceKind.EntitySet)]
+        // singleton segment should return the singleton
         [InlineData("VipCustomer", "VipCustomer", EdmNavigationSourceKind.Singleton)]
         [InlineData("VipCustomer/Name", "VipCustomer", EdmNavigationSourceKind.Singleton)]
+        // navigation property segment after a singleton should return the navigation property's entity set
         [InlineData("VipCustomer/Orders", "Orders", EdmNavigationSourceKind.EntitySet)]
         [InlineData("VipCustomer/NS.IsUpgraded", null, EdmNavigationSourceKind.None)]
+        // type segment should return the corresponding navigation source
         [InlineData("Customers(1)/NS.SpecialCustomer", "Customers", EdmNavigationSourceKind.EntitySet)]
         [InlineData("VipCustomer/NS.SpecialCustomer", "VipCustomer", EdmNavigationSourceKind.Singleton)]
+        // coung segment should return null
         [InlineData("Customers/$count", null, EdmNavigationSourceKind.None)]
+        // value segment should return null
         [InlineData("Customers(1)/Account/Amount/$value", null, EdmNavigationSourceKind.None)]
+        // batch segment should return null
         [InlineData("$batch", null, EdmNavigationSourceKind.None)]
+        // metadata segment should return null
         [InlineData("$metadata", null, EdmNavigationSourceKind.None)]
         public void GetNavigationSource_ReturnsCorrectNavigationSource(string path, string expectedNavigationSource, EdmNavigationSourceKind navigationSourceKind)
         {
