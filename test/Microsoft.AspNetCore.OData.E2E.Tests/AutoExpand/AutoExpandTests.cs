41988cde4b538af33fd7a0c360304636f8b81092
Samuel Wanjohi <sam.wanjohi94@gmail.com>
2024-08-15 17:46:09 +0300

    Add tests for Select and Expand without $ prefix

--- test/Microsoft.AspNetCore.OData.E2E.Tests/AutoExpand/AutoExpandTests.cs
+++ test/Microsoft.AspNetCore.OData.E2E.Tests/AutoExpand/AutoExpandTests.cs
@@ -121,43 +121,14 @@ namespace Microsoft.AspNetCore.OData.E2E.Tests.AutoExpand
         }
 
         [Theory]
-        [InlineData("$Expand=Order,HomeAddress/CountryOrRegion&$Select=Id", 4)]
-        [InlineData("$Expand=HomeAddress/CountryOrRegion,HomeAddress/Microsoft.AspNetCore.OData.E2E.Tests.AutoExpand.UsAddress/ZipCode,Order", 4)]
-        [InlineData("$Expand=Friend", 4)]
-        public async Task QueryForResources_WithExpandQueryParamsCapitalized_IncludesAutoExpandNavigationProperty(string queryParams, int propCount)
-        {
-            // Arrange
-            string queryUrl = $"autoexpand/Customers(5)?{queryParams}";
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, queryUrl);
-            request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json;odata.metadata=none"));
-            HttpClient client = CreateClient();
-
-            HttpResponseMessage response = null;
-            Exception exception = null;
-
-            // Act
-            try
-            {
-                response = await client.SendAsync(request);
-            }
-            catch (Exception ex)
-            {
-                exception = ex;
-            }
-
-            // Assert
-            Assert.Null(exception);
-            Assert.NotNull(response);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.NotNull(response.Content);
-
-            await VerifyNavigationPropertiesAndLevels(response, propCount);
-        }
-
-        [Theory]
+        [InlineData("$Expand=Order & Select=Friend", 3)]
+        [InlineData("Expand=Order & $Select=Order", 3)]
         [InlineData("$Expand=Order,HomeAddress/CountryOrRegion($Select=Name)", 4)]
-        [InlineData("$Expand=HomeAddress/CountryOrRegion,HomeAddress/Microsoft.AspNetCore.OData.E2E.Tests.AutoExpand.UsAddress/ZipCode,Order($Expand=Choice($Select=Amount))", 4)]
-        public async Task QueryForResources_WithExpandSelectQueryParamsCapitalized_IncludesAutoExpandNavigationProperty_(string queryParams, int propCount)
+        [InlineData("$Expand=HomeAddress/CountryOrRegion,HomeAddress/Microsoft.AspNetCore.OData.E2E.Tests.AutoExpand.UsAddress/ZipCode,Order($Expand=Choice)", 4)]
+        [InlineData("Expand=HomeAddress/Microsoft.AspNetCore.OData.E2E.Tests.AutoExpand.UsAddress/ZipCode", 4)]
+        [InlineData("Expand=Friend(Select=Order) & $Select=Order", 3)]
+        [InlineData("Select=Order & Expand=Order(Expand=Choice(Select=Amount))", 3)]
+        public async Task QueryForResources_WithExpandSelectQueryParamsCapitalized_IncludesAutoExpandNavigationProperty(string queryParams, int propCount)
         {
             // Arrange
             string queryUrl = $"autoexpand/Customers(5)?{queryParams}";
@@ -593,6 +564,7 @@ namespace Microsoft.AspNetCore.OData.E2E.Tests.AutoExpand
         private async Task VerifyNavigationPropertiesAndLevels(HttpResponseMessage response, int propCount)
         {
             var customer = await response.Content.ReadAsObject<JObject>().ConfigureAwait(false);
+            this.output.WriteLine(customer.ToString());
             Assert.Equal(customer.Properties().Count(), propCount);
             VerifyOrderAndChoiceOrder(customer);
             VerifyHomeAddress(customer);
