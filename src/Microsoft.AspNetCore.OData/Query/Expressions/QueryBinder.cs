f228701be87a7fbe043b3955895ae791aee5a92a
John Gathogo <john.gathogo@microsoft.com>
2024-10-30 09:17:37 +0300

    Remove misplaced using statement

--- src/Microsoft.AspNetCore.OData/Query/Expressions/QueryBinder.cs
+++ src/Microsoft.AspNetCore.OData/Query/Expressions/QueryBinder.cs
@@ -24,7 +24,6 @@ using Microsoft.OData;
 using Microsoft.OData.Edm;
 using Microsoft.OData.ModelBuilder;
 using Microsoft.OData.UriParser;
-using Microsoft.VisualBasic;
 
 namespace Microsoft.AspNetCore.OData.Query.Expressions;
 
