4c2e5725568f401ad1594569235bb32777d2c48f
Sam Xu <saxu@microsoft.com>
2025-06-05 11:58:09 -0700

    Fixes #1483: Regression with computed in $orderby with 'Could not find a property named xxx on ....' (#1486)
    * Fixes #1483: Regression with computed in $orderby with 'Could not find a property named xxx on ....'
    
    * Bump version to 9.3.2

--- test/Microsoft.AspNetCore.OData.E2E.Tests/DollarCompute/DollarComputeDataModel.cs
+++ test/Microsoft.AspNetCore.OData.E2E.Tests/DollarCompute/DollarComputeDataModel.cs
@@ -49,3 +49,12 @@ public class ComputeSale
 
     public IDictionary<string, object> Dynamics { get; set; } = new Dictionary<string, object>();
 }
+
+public class ComputeShopper
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
